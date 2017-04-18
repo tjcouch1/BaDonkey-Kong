312db01f5c59967107ff39729babf753f239b0ab
MOLLY-PC\Molly <mks0019@uah.edu>
2017-04-18 18:29:06 -0500

    worked on main menu and other scenes

---
+++ Assets/Scripts/ButtonNavigation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonNavigation : MonoBehaviour {
+	public int index=0;
+	public int totallevels=2;
+	public float yOffset=1f;
+	// Use this for initialization
+
+	// Update is called once per frame
+
+	public void LoadScence (int level)
+	{
+
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			if(index<=totallevels-1)
+			{
+				index++;
+				Vector2 position=transform.position;
+				position.y-= yOffset;
+				transform.position=position;
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			if(index>0)
+			{
+				index--;
+				Vector2 position=transform.position;
+				position.y+= yOffset;
+				transform.position=position;
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.Return))
+		{
+			if(index==0){
+				SceneManager.LoadScene(level);
+			}
+			else 
+				SceneManager.LoadScene(level);
+
+		}
+	}
+}
---
+++ Assets/Scripts/LoadonClick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadonClick : MonoBehaviour {
+	public void LoadScence (int level)
+	{
+		SceneManager.LoadScene(level);
+	}
+}
