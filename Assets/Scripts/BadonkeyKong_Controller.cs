ac4c441837830f49daa235c8547ef954df1fd6ed
Clark Burton Chambers <clarkchambers94@gmail.com>
2017-04-25 15:46:43 -0500

    Working penguin added! Don’t have random idle working yet. Going to do that last probably

---
+++ Assets/Scripts/BadonkeyKong_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadonkeyKong_Controller : MonoBehaviour {
+
+	public GameObject waypoint_nav;
+	GameObject current_nav;
+	BoxCollider box_col;
+	CapsuleCollider cap_col;
+
+	// Use this for initialization
+	void Awake () {
+		box_col = GetComponent<BoxCollider>();
+		cap_col = GetComponent<CapsuleCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
