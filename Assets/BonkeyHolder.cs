6bfeab0b92f0eac72abc4f553aadb3c204b39a69
Clark Burton Chambers <clarkchambers94@gmail.com>
2017-04-26 17:39:50 -0500

    Oops. Forgot these.

---
+++ Assets/BonkeyHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonkeyHolder : MonoBehaviour {
+
+	int num_badonkeys = 0;
+	int num_deaths = 0;
+
+	// Use this for initialization
+	void Start () {
+		num_badonkeys = gameObject.GetComponentsInChildren<BKControllerController>().Length;
+	}
+	
+	public void kill_badonkey() 
+	{ 
+		num_deaths++; 
+		Debug.Log("Killed Bonkey: " + num_deaths);
+	}
+	public bool isKeySpawn () 
+	{ 
+		return num_deaths >= num_badonkeys; 
+	}
+}
---
+++ Assets/Scripts/KillMeTime.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillMeTime : MonoBehaviour {
+
+	public float killtime = 2.0f;
+	public GameObject key;
+	public float keyforce = 3.0f;
+	float toKill = 0.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		toKill += Time.deltaTime;
+		if(toKill > killtime) {
+			BonkeyHolder bh = transform.parent.gameObject.GetComponent<BonkeyHolder>();
+			bh.kill_badonkey();
+			if(bh.isKeySpawn()) {
+				Debug.Log("SPAWNING KEY");
+				Transform t = Instantiate(key).transform;
+				t.position = transform.position;
+
+				Rigidbody rb = t.gameObject.GetComponent<Rigidbody>();
+				rb.velocity = new Vector3(0, keyforce, 0);
+				rb.angularVelocity = new Vector3(keyforce * 33, keyforce * 33, 0);
+			}
+			Destroy(gameObject);
+		}
+	}
+}
